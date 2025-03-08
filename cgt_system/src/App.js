328f562f103731ece48425fc561b0fa05242688d
KS_REACT_SE170505 <anntse170505@fpt.edu.vn>
2025-03-08 16:10:13 +0700

    ChatBot

--- cgt_system/src/App.js
+++ cgt_system/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import MainRoutes from "./app/routes/MainRoutes";
 import "./app/styles/GlobalStyles/GlobalStyles.scss";
+import Chatbot from "./app/components/ChatBot/ChatBot";
 
 export default function App() {
-  return <MainRoutes />;
+  return (<> {/* Fragment để bọc MainRoutes và Chatbot */}
+    <MainRoutes />
+    <Chatbot />
+  </>
+  );
 }
