34d61bd9d9bea4068498387ac65f4199003df59a
quyen-tv <tvq05112003@gmail.com>
2025-03-08 11:13:33 +0700

    Handle catch global token expired error

--- cgt_system/src/index.js
+++ cgt_system/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { setupInterceptors } from "./app/config/axiosInstance";
+
+setupInterceptors();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
